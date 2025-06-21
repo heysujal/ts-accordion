9bf66b69f85c48c5a4813ead7337e2c778cef028
Sujal Gupta <sujalgupta6100@gmail.com>
2025-06-22 01:06:04 +0530

    Refactor Home component structure and restore Accordion functionality
    Signed-off-by: Sujal Gupta <sujalgupta6100@gmail.com>

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from "react";
-import Accordion from "../components/accordion";
+import Accordion from "./accordion";
 
 export default function Home(){
     const [multiOpen, setMultiOpen] = useState(true);
